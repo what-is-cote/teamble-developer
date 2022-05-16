5989fe7939e6c00abaa2c8907c6976bb76839dab
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-05-16 18:10:02 +0800

    week09

---
+++ gyuwonMoon/백준/solved.ac/class1/1000 A+B/code.py
@@ -0,0 +1,6 @@
+import sys, os
+sys.stdin=open(f'{os.path.dirname(os.path.realpath(__file__))}/input.txt', "rt")
+
+a, b=map(int, input().split())
+
+print(a+b)
---
+++ gyuwonMoon/백준/solved.ac/class1/2438 별찍기 - 1/code.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.stdin=open(f'{os.path.dirname(os.path.realpath(__file__))}/input.txt', "rt")
+
+n=int(input())
+
+for i in range(1, n+1):
+  for _ in range(i):
+    print('*', end='')
+  print()
