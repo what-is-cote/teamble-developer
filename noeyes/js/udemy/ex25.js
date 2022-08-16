d768e0ed66ba8888f7f8a56c16220e1a8ef07075
n0eyes <doll4586633@naver.com>
2022-08-16 14:06:15 +0900

    udemy ex24 ~ ex25

---
+++ noeyes/js/udemy/ex25.js
@@ -0,0 +1,43 @@
+// Binary Search Exercise
+// Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+// This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+
+// Examples:
+
+// binarySearch([1,2,3,4,5],2) // 1
+// binarySearch([1,2,3,4,5],3) // 2
+// binarySearch([1,2,3,4,5],5) // 4
+// binarySearch([1,2,3,4,5],6) // -1
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 10) // 2
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 95) // 16
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 100) // -1
+
+function binarySearch(arr, n) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (arr[middle] === n) {
+      return middle;
+    } else if (arr[middle] < n) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  ls;
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3], 3));
