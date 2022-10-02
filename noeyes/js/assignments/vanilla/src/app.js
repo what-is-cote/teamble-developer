e77eed16121dead9d536669c8433547d09dece32
n0eyes <doll4586633@naver.com>
2022-10-02 16:46:08 +0900

    vanilla - 생명주기 추가

--- noeyes/js/assignments/vanilla/src/app.js
+++ noeyes/js/assignments/vanilla/src/app.js
@@ -5,8 +5,6 @@ export default class App extends Component {
   constructor() {
     const $app = document.querySelector("#app");
     super($app);
-
-    new Items(document.querySelector(".box1"));
   }
 
   template() {
@@ -15,6 +13,14 @@ export default class App extends Component {
         <span>before component</span>
       </div>
     `;
+  }
+
+  mounted() {
+    new Items(document.querySelector(".box1"));
+  }
+
+  setEvent() {
+    this.addEvent("click", ".box1", () => this.setState({}));
   }
 }
 
--- noeyes/js/assignments/vanilla/src/core/Component.js
+++ noeyes/js/assignments/vanilla/src/core/Component.js
@@ -7,12 +7,14 @@ export default class Component {
     this.render();
     this.setEvent();
   }
+  mounted() {}
   setup() {}
   template() {
     return "";
   }
   render() {
-    this.$target.innerHTML = this.$target.innerHTML + this.template();
+    this.$target.innerHTML = this.template();
+    this.mounted();
   }
   setEvent() {}
   setState(newState) {
@@ -22,9 +24,7 @@ export default class Component {
 
   addEvent(eventType, selector, callback) {
     const children = [...this.$target.querySelectorAll(selector)];
-    console.log(children, selector);
-    // selector에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
-    // closest를 이용하여 처리한다.
+
     const isTarget = (target) =>
       children.includes(target) || target.closest(selector);
     this.$target.addEventListener(eventType, (event) => {
