5380fb3728a87dc8cffb812fd4c999a72250e008
n0eyes <doll4586633@naver.com>
2022-10-05 05:49:07 +0900

    2022 카카오 블라인드 채용 k진수에서 소수 개수 구하기

--- noeyes/js/programmers/level2/kakao/tempCodeRunnerFile.js
+++ noeyes/js/programmers/level2/kakao/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-
-  // queue1
-  //   .concat(queue2)
-  //   .sort((a, b) => b - a)
-  //   .forEach((e) => {
-  //     if (check[0] + e <= (sum1 + sum2) / 2) check[0] += e;
-  //     else check[1] += e;
-  //   });
-
-  // if (check[0] !== check[1]) return -1;
+subNumber <= 1) 
