127bbd2cd65a00f9048b48158924570ca1d03dbf
n0eyes <doll4586633@naver.com>
2022-10-14 23:54:11 +0900

    js, lodash 메소드 구현

---
+++ noeyes/js/assignments/vanilla/lodash/lodash.js
@@ -0,0 +1,180 @@
+Array.prototype.myReduce = function (callback, init) {
+  let prev = init ? init : this[0];
+
+  for (let i = init == null ? 1 : 0; i < this.length; i++) {
+    prev = callback(prev, this[i], i, this);
+  }
+
+  return prev;
+};
+
+Array.prototype.myFilter = function (callback) {
+  const arr = [];
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this) && arr.push(this[i]);
+  }
+
+  return arr;
+};
+
+var testArray = [
+  {
+    description: "Random description.",
+    testNumber: 123456789,
+    testBoolean: true,
+    testObject: {
+      testString: "test string",
+      testNumber: 12345,
+    },
+    testArray: [
+      {
+        myName: "test name",
+        myNumber: 123245,
+      },
+    ],
+  },
+  {
+    description: "Random description.",
+    testNumber: 123456789,
+    testBoolean: true,
+    testObject: {
+      testString: "test string",
+      testNumber: 12345,
+    },
+    testArray: [
+      {
+        myName: "test name",
+        myNumber: 123245,
+      },
+    ],
+  },
+];
+var a = {
+  test: [1],
+  love: { id: 3, mylove: { name: "yj", age: 25 } },
+  n: 11,
+};
+const deepCopy = function (obj) {
+  const copiedObj = {};
+  function recursiveCopy(data) {
+    const local = {};
+    if (Array.isArray(data)) return data.map((e) => recursiveCopy(e));
+    else if (data && typeof data === "object") {
+      for (let k in data) local[k] = recursiveCopy(data[k]);
+      return local;
+    } else return data;
+  }
+
+  for (let k in obj) {
+    copiedObj[k] = recursiveCopy(obj[k]);
+  }
+
+  return copiedObj;
+};
+
+function renameKey(obj, old_key, new_key) {
+  if (old_key !== new_key) {
+    Object.defineProperty(
+      obj,
+      new_key,
+      Object.getOwnPropertyDescriptor(obj, old_key)
+    );
+    delete obj[old_key];
+  }
+}
+
+/**
+ *
+ * @param {string} tag
+ * @param {Document | HTMLElement} target
+ */
+function qsAll(tag, target = document) {
+  const result = [];
+
+  const selectNode = (parent) => {
+    Array.from(parent.children).forEach((node) => {
+      if (node.tagName === tag.toUpperCase()) result.push(node);
+
+      selectNode(node);
+    });
+  };
+
+  selectNode(target);
+
+  return result;
+}
+
+console.log(qsAll("button"));
+
+const MyPromise2 = class {
+  constructor(executor) {
+    this.state = "pending";
+    this.laterCalls = [];
+    this.decidePromiseByMethod.bind(this);
+    this.applyChangedState.bind(this);
+
+    try {
+      executor(this.resolve.bind(this), this.reject.bind(this));
+    } catch (error) {
+      this.reject(error);
+    }
+  }
+
+  applyChangedState(value, state) {
+    if (!(this.state === "pending")) return;
+    if (value instanceof MyPromise2) {
+      value.then((innerPromiseValue) => {
+        this.value = innerPromiseValue;
+        this.status = state;
+        this.laterCalls.forEach((latercall) => latercall());
+      });
+    } else {
+      this.value = value;
+      this.state = state;
+
+      this.laterCalls.forEach((latercall) => latercall());
+    }
+  }
+
+  decidePromiseByMethod(method, callback) {
+    console.log("decide");
+    const state = method === "then" ? "resolved" : "rejected";
+    if (this.state === state)
+      return new MyPromise2((resolve) => resolve(callback(this.value)));
+
+    if (this.state === "pending")
+      return new MyPromise2((resolve) => {
+        this.laterCalls.push(() => resolve(callback(this.value)));
+      });
+    return this;
+  }
+
+  resolve(value) {
+    this.applyChangedState(value, "resolved");
+  }
+
+  reject(value) {
+    this.applyChangedState(value, "rejected");
+  }
+
+  then(callback) {
+    console.log("then");
+    return this.decidePromiseByMethod("then", callback);
+  }
+
+  catch(callback) {
+    return this.decidePromiseByMethod("catch", callback);
+  }
+};
+
+(async () => {
+  const data = await new MyPromise2((resolve, reject) => {
+    setTimeout(() => {
+      console.log("first");
+      resolve("before");
+    }, 2000);
+  });
+
+  console.log(data);
+  console.log("after");
+})();
